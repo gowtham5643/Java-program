55fc5cc66e3180ec39cab322a4a40cb37040d87e
gowtham5643 <111501086+gowtham5643@users.noreply.github.com>
2023-12-30 12:19:40 +0530

    Create ThreadExample.Java

---
+++ Day 5/ThreadExample.Java
@@ -0,0 +1,18 @@
+package placement_training;
+class MyRunnable implements Runnable {
+    @Override
+    public void run() {
+        String threadName = Thread.currentThread().getName();
+        System.out.println("Thread Name: " + threadName);
+    }
+}
+
+public class ThreadExample {
+    public static void main(String[] args) {
+        MyRunnable myRunnable = new MyRunnable();
+        Thread t1 = new Thread(myRunnable);
+        Thread t2 = new Thread(myRunnable);
+        t1.start();
+        t2.start();
+    }
+}
